93f477af511badbaba46ea08c0c92effe0214853
siddharthan21 <siddharthan44@gmail.com>
2023-09-20 08:25:47 +0530

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-$(".card").hover(function () {
-        // $("ul").addClass("dis");
-        // console.log(this *"ul");
-        // console.log($(this).children().eq(1));
-        $(this).children().eq(1).addClass("dis");
-        
-    }, function () {
-        // out
-        // $("ul").removeClass("dis");
-        $(this).children().eq(1).removeClass("dis");
+$(".click").click(function (e) { 
+    if($(".menu-nav").css("display")=="none"){
+    $(".menu-nav").css("display", "flex");
+    }else{
+    $(".menu-nav").css("display", "none");
     }
-);
+    
+});
